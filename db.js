0be081db3f999c37fb1d958a60c0f7f4cb795302
jeacqueline.rios-srwity <jeacqueline.rios@outlook.com>
2023-04-07 20:28:12 -0600

    modified host and port with env

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: "localhost",
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database:process.env.DB_NAME,
-    port:3306, 
+    port:process.env.DB_PORT, 
 }
 );
 db.connect((err)=>{
